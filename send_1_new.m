b2facc0ff05f94d9a04be687392a1384c178c316
JarodBoone <jtboone12@gmail.com>
2020-05-12 18:42:56 -0400

    redid if statements

--- send_1_new.m
+++ send_1_new.m
@@ -30,47 +30,44 @@ if isempty(data)
     new_data = data;
 end
 
+%%%%%%%%%%%%%%%%%%
 if data(1,1) == 0 
     if data(1,2) <= 0
-        if data(1,3)-1 > 0
-            new_data(1,3) = data(1,3) - 1;
-        else
-            new_data(1,3) = 0; % End the timer
-            new_data(1,2) = pause; % Add a startup delay 
+        if new_data(4) > 0 % times to repeat 
+            if data(1,3)-1 > 0 % ticks to send
+                new_data(1,3) = data(1,3) - 1; 
 
+                % Get bits from this chunk 
+                if (msg(1) == 0) b1 = -1; 
+                else b1 = 1; end 
+                if (msg(2) == 0) b2 = -1; 
+                else b2 = 1; end 
+
+                % send the signal 
+                signal_point = ((b1 * sin(2*pi()*f1*t(1,n)) + ...
+                    b2 * sin(2*pi()*f2*t(1,n)))/(1.22)); 
+            else
+
+                % we have finished this chunk 
+                new_data(1,3) = 0; % End the timer
+                new_data(1,2) = pause; % Add a startup delay 
+                new_data(1,4) = new_data(1,4) - 1; % decrement repetitino
+            end   
+        else
+            % get a new message 
             if length(new_msg) >= 3
                 new_msg = msg(3:end);
             else
-                % End broadcast 
+                % End broadcast done with message 
                 new_data(1) = 1;
             end
-        end
-        if data(1,3) >= 0 
-            % Attempt to repeat same 4 points
-            if new_data(4) == 4
-                if (length(msg) >= 3) 
-                    if (msg(1) == 0) b1 = -1; 
-                    else b1 = 1; end 
-                    if (msg(2) == 0) b2 = -1; 
-                    else b2 = 1; end 
-                else
-                    b1 = 1; 
-                    b2 = 1; 
-                end 
-                new_data(5) = b1;
-                new_data(6) = b2;
-            end
-            if new_data(4) > 0
-                signal_point = ((new_data(5) * sin(2*pi()*f1*t(1,n)) + ...
-                    new_data(6) * sin(2*pi()*f2*t(1,n)))/(1.22)); 
-                
-                new_data(4) = new_data(4) - 1;
-            end
-            new_data(4) = repeat;
-            % countdown 3rd data index  
-        end
-    else
+
+            % start data(4) at its value 
+            new_data(1,4) = repeat; 
+
         
+        end
+    else 
         % Countdown startup delay
         if data(1,2)-1 > 0
             new_data(1,2) = data(1,2) - 1;
@@ -79,6 +76,9 @@ if data(1,1) == 0
             new_data(1,2) = 0;
             new_data(1,3) = send_steps;
         end
-    end
+    end 
 end
+%%%%%%%%%%%%%%%
 end
+
+
